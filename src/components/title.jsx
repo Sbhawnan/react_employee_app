eaaf3de56e294267a972b9a606ad122476a77c9c
Sbhawnan <sbhawnan@nyit.edu>
2024-04-04 18:40:37 -0400

    first commit

---
+++ src/components/title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Title = (props) => {
+    return (
+        <h1>{props.name} {props.lastname}</h1>
+    );
+
+}
+
+export default Title;
---
+++ src/data/data.js
@@ -0,0 +1,57 @@
+const data = [{
+    "employeeId": "12345",
+    "firstName": "Alice",
+    "lastName": "Johnson",
+    "email": "alice.johnson@example.com",
+    "position": "Software Developer",
+    "department": "Engineering",
+    "location": "Office",
+    "status": "Active"
+  },
+
+  {
+    "employeeId": "67890",
+    "firstName": "Bob",
+    "lastName": "Williams",
+    "email": "bob.williams@example.com",
+    "position": "Marketing Specialist",
+    "department": "Marketing",
+    "location": "Remote",
+    "status": "Active"
+  },
+
+  {
+    "employeeId": "13579",
+    "firstName": "Eva",
+    "lastName": "Brown",
+    "email": "eva.brown@example.com",
+    "position": "HR Manager",
+    "department": "Human Resources",
+    "location": "Office",
+    "status": "Active"
+  },
+
+  {
+    "employeeId": "24680",
+    "firstName": "David",
+    "lastName": "Lee",
+    "email": "david.lee@example.com",
+    "position": "Financial Analyst",
+    "department": "Finance",
+    "location": "Office",
+    "status": "Active"
+  },
+
+  {
+    "employeeId": "97531",
+    "firstName": "Sophia",
+    "lastName": "Garcia",
+    "email": "sophia.garcia@example.com",
+    "position": "Customer Support Representative",
+    "department": "Support",
+    "location": "Remote",
+    "status": "Active"
+  },
+]
+
+export default data;
